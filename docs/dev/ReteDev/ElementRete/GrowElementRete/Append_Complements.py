838a2595544e16a3623edd64ae587354736a1a41
betaglot <christopheralduncan@gmail.com>
2026-02-26 15:49:21 -0500

    Get to a Myriad

--- docs/dev/ReteDev/ElementRete/GrowElementRete/Append_Complements.py
+++ docs/dev/ReteDev/ElementRete/GrowElementRete/Append_Complements.py
@@ -1,4 +1,10 @@
 import pandas as pd
+from datetime import datetime
+
+
+current_datetime = datetime.now()
+
+timecode = current_datetime.strftime("%y%m%d-%H%M%S")
 
 
 def csv_to_dict_pandas(filename):
@@ -20,28 +26,67 @@ data_pandas = csv_to_dict_pandas(
 
 def AddComplement(RelationshipClass, ComplementRelationshipClass):
 
-    for row in data_pandas:
+    with open(
+        rf"C:\Users\chris\plantabeta\docs\dev\ReteDev\ElementRete\GrowElementRete\Output\{timecode}_ElementRete.csv",
+        "a",
+        newline="",
+    ) as f:
 
-        if row["_Relationship_"] == RelationshipClass:
+        for row in data_pandas:
 
-            print(f"{row['_Target_']},{ComplementRelationshipClass},{row['_Origin_']}")
+            # ToDo: Check to see if the complement already exists in data_pandas
+            if row["_Relationship_"] == RelationshipClass:
 
-        if row["_Relationship_"] == ComplementRelationshipClass:
+                f.write(
+                    f"{row['_Target_']},{ComplementRelationshipClass},{row['_Origin_']}\n"
+                )
 
-            print(f"{row['_Target_']},{RelationshipClass},{row['_Origin_']}")
+            if row["_Relationship_"] == ComplementRelationshipClass:
+
+                f.write(f"{row['_Target_']},{RelationshipClass},{row['_Origin_']}\n")
 
 
-# AddComplement("IsSuperTaxonOf","IsSubTaxonOf")
+#
+#
+#           Run the Code
+#
+#
 
-# AddComplement("_Has_The_Possible_Ekon_","_Is_A_Possible_Ekon_Of_")
-# AddComplement("_Is_A_SuperAktion_Of_","_Is_A_SubAktion_Of_")
-# AddComplement("_Is_A_SuperPart_Of_","_Is_A_SubPart_Of_")
-# AddComplement("_Is_A_SuperTaxon_Of_","_Is_A_SubTaxon_Of_")
-# AddComplement("_Has_The_Possible_Tool_","_Is_The_Possible_Tool_Of_")
-# AddComplement("_Has_The_Possible_Variant_","_Is_The_Possible_Variant_Of_")
-# AddComplement("_Requires_The_Somon_","_Is_The_Required_Somon_Of_")
-# AddComplement("_Has_The_SubVion_","_Is_The_SubVion_Of_")
-# AddComplement("_Has_The_Attribute_","_Is_An_Attribute_Of_")
-# AddComplement("_Has_The_Method_","_Is_A_Method_Of_")
+# Possible Element from OtherFaktor
+AddComplement("_Has_The_Possible_Labon_", "_Is_A_Possible_Labon_Of_")
+AddComplement("_Has_The_Possible_Somon_", "_Is_A_Possible_Somon_Of_")
+AddComplement("_Has_The_Possible_Ekon_", "_Is_A_Possible_Ekon_Of_")
+AddComplement("_Has_The_Possible_Vion_", "_Is_A_Possible_Vion_Of_")
+AddComplement("_Has_The_Possible_Moton_", "_Is_A_Possible_Moton_Of_")
+AddComplement("_Has_The_Possible_Animon_", "_Is_A_Possible_Animon_Of_")
 
-AddComplement("_Has_The_Possible_Somon_","_Is_A_Possible_Somon_Of_")
+# Requires Element from OtherFaktor
+AddComplement("_Requires_The_Labon_", "_Is_A_Required_Labon_Of_")
+AddComplement("_Requires_The_Somon_", "_Is_A_Required_Somon_Of_")
+AddComplement("_Requires_The_Ekon_", "_Is_A_Required_Ekon_Of_")
+AddComplement("_Requires_The_Vion_", "_Is_A_Required_Vion_Of_")
+AddComplement("_Requires_The_Moton_", "_Is_A_Required_Moton_Of_")
+AddComplement("_Requires_The_Animon_", "_Is_A_Required_Animon_Of_")
+
+# Aktion
+AddComplement("_Is_A_SuperAktion_Of_", "_Is_A_SubAktion_Of_")
+
+# Tool
+AddComplement("_Has_The_Possible_Tool_", "_Is_The_Possible_Tool_Of_")
+
+# Partonomy
+AddComplement("_Is_A_SuperPart_Of_", "_Is_A_SubPart_Of_")
+AddComplement("_Has_The_SubVion_", "_Is_The_SubVion_Of_")
+
+# Taxonomy
+AddComplement("_Is_A_SuperTaxon_Of_", "_Is_A_SubTaxon_Of_")
+AddComplement("_Is_An_Ancestor_Of_", "_Has_The_Ancestor_")
+
+# Methods
+AddComplement("_Has_The_Method_", "_Is_A_Method_Of_")
+
+# Attributes
+AddComplement("_Has_The_Attribute_", "_Is_An_Attribute_Of_")
+
+# Variant
+AddComplement("_Has_The_Possible_Variant_", "_Is_The_Possible_Variant_Of_")
---
+++ docs/dev/ReteDev/ElementRete/GrowElementRete/Grow_From_Betoms.py
@@ -0,0 +1,3 @@
+# Use Properties-Betoms.json to Grow ElementRete
+# If the elements appear together in a Betom, then there is a connection, 
+# use the Hyphenated Betom or the Betomglyf Dictionary
