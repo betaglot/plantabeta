e20df61dc58492e10c6b242142ceeca4ff40e993
BETAGLOT <christopheralduncan@gmail.com>
2026-07-29 23:09:25 +0000

    Python

---
+++ docs/dev/DesignBox/Get_All_DesignBox.py
@@ -0,0 +1,15 @@
+# This is easiest if done locally
+# 
+# Create a Python Script which can search through all DesignBoxes the reference directory
+
+# Between
+
+# Create a python script which searches through the directory called
+
+# "::: details &"
+
+
+#
+# 
+# 
+#  ":::"
